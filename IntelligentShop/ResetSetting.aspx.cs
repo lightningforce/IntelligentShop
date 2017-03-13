830a52a21346cfe4f8e52681196a11a76d727703
lightningforce <sorth_27@hotmail.com>
2017-03-13 22:24:38 +0700

    edit master page

---
+++ IntelligentShop/ResetSetting.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using BSD.Dal;
+namespace IntelligentShop
+{
+    public partial class ResetSetting : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            using (DataAccess dac = new DataAccess())
+            {
+                dac.Open(Provider.MSSQL);
+                DbCommand cmd = dac.CreateCommand("usp_Reset");
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
